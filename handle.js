88c818e6108996f2f50cc2c08e189fa6f2051ef1
riha7 <harik0725@gmail.com>
2021-07-23 11:54:45 +0530

    completed Navbar

--- handle.js
+++ handle.js
@@ -5,3 +5,24 @@ function HandleButtons()
     home.style.padding='14px'
     home.style.borderRadius="10px"
 }
+function HandleAbout()
+{
+    var b=document.getElementById("about")
+    b.style.backgroundColor='green'
+    b.style.padding='14px'
+    b.style.borderRadius="10px"
+}
+function HandleService()
+{
+    var s=document.getElementById("service")
+    s.style.backgroundColor='green'
+    s.style.padding='14px'
+    s.style.borderRadius="10px"
+}
+function HandleContact()
+{
+    var c=document.getElementById("contact")
+    c.style.backgroundColor='green'
+    c.style.padding='14px'
+    c.style.borderRadius="10px"
+}
