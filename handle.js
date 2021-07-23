b31fdf67e588eac2c7e35bd7172f08b9755d48fa
riha7 <harik0725@gmail.com>
2021-07-23 11:42:59 +0530

    worked only Home Button

---
+++ handle.js
@@ -0,0 +1,7 @@
+function HandleButtons()
+{
+    var home=document.getElementById("home")
+    home.style.backgroundColor='green'
+    home.style.padding='14px'
+    home.style.borderRadius="10px"
+}
